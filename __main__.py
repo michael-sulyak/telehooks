a2421b928ee820571b00f55928d0513725047c76
Michael Sulyak <6431441+michael-sulyak@users.noreply.github.com>
2024-08-12 23:59:16 +0300

    Fix working with loop

--- __main__.py
+++ __main__.py
@@ -22,6 +22,7 @@ sentry_sdk.init(
     integrations=(AioHttpIntegration(),),
 )
 
+loop = asyncio.new_event_loop()
 amqp_connection = None
 
 
@@ -29,10 +30,8 @@ async def init_handlers(app: web.Application) -> None:
     global amqp_connection
 
     logging.info('Initializing AMQP connection...')
-    amqp_connection = await utils.connect_robust_to_mq(config.AMQP_URL, timeout=60)
+    amqp_connection = await utils.connect_robust_to_mq(config.AMQP_URL, loop=loop, timeout=60)
     logging.info('AMQP connection established.')
-
-    amqp_channel = await amqp_connection.channel()
 
     ip = await utils.get_my_ip()
 
@@ -60,14 +59,21 @@ async def init_handlers(app: web.Application) -> None:
 
     def _create_handler(routing_key: str) -> typing.Callable:
         async def _handle(request: web.Request) -> web.Response:
-            await amqp_channel.default_exchange.publish(
-                aio_pika.Message(await request.read(), expiration=config.AMQP_MSG_EXPIRATION),
-                routing_key=routing_key,
-            )
+            amqp_channel = await amqp_connection.channel()
+
+            try:
+                await amqp_channel.default_exchange.publish(
+                    aio_pika.Message(await request.read(), expiration=config.AMQP_MSG_EXPIRATION),
+                    routing_key=routing_key,
+                )
+            finally:
+                await amqp_channel.close()
 
             return web.Response()
 
         return _handle
+
+    amqp_channel = await amqp_connection.channel()
 
     for bot_slug, bot in config.BOTS.items():
         endpoint_for_webhook = str(uuid.uuid4())
@@ -81,19 +87,23 @@ async def init_handlers(app: web.Application) -> None:
         app.on_startup.append(_create_on_startup(bot, webhook_url))
         app.on_shutdown.append(_create_on_shutdown(bot))
 
+    await amqp_channel.close()
+
 
 async def on_startup(app: web.Application) -> None:
     logging.info('Starting...')
 
 
 async def on_shutdown(app: web.Application) -> None:
+    logging.info('Stopping...')
+
     if amqp_connection is not None:
         await amqp_connection.close()
 
 
 def main() -> typing.NoReturn:
     logging.info('Getting the current IP... ')
-    ip = asyncio.run(utils.get_my_ip())
+    ip = loop.run_until_complete(utils.get_my_ip())
     logging.info('Current IP: %s', ip)
 
     logging.info('Generating SSL certificate...')
@@ -108,7 +118,7 @@ def main() -> typing.NoReturn:
     app.on_startup.append(on_startup)
     app.on_shutdown.append(on_shutdown)
 
-    asyncio.run(init_handlers(app))
+    loop.run_until_complete(init_handlers(app))
 
     web.run_app(
         app,
@@ -118,6 +128,7 @@ def main() -> typing.NoReturn:
             ssl_key_path=config.SSL_KEY_PATH,
             ssl_cert_path=config.SSL_CERT_PATH,
         ),
+        loop=loop,
     )
 
 
